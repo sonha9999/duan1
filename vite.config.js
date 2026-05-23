01927a0637b50de44489cccb01e9ad9f9c9f2c48
sonha9999 <sonlamxxx.9999@gmail.com>
2026-05-23 17:34:41 +0700

    Thêm cấu hình tự động deploy

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/thachcao/", // Thêm chính xác dòng này để định tuyến đúng trên GitHub Pages
+  base: "/duan1/", // Thêm chính xác dòng này để định tuyến đúng trên GitHub Pages
 });
