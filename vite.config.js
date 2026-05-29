a313239154bbf5eeb2d4e994e5d41c4b2c9eb60a
sonha9999 <sonlamxxx.9999@gmail.com>
2026-05-29 14:29:33 +0700

    chot

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  // Tự động nhận diện: Nếu chạy trên Netlify (hoặc máy cục bộ) thì dùng '/', nếu chạy trên GitHub Actions thì dùng '/duan1/'
-  base: process.env.NETLIFY ? "/" : "/duan1/",
-});
